6ca3e4bfa3985b28efbf2bdaf1adab75875c9452
cesartidrick <cesartidrick@gmail.com>
2022-04-28 08:58:29 -0600

    commit tree 665b0ddc5aed1ff88f0bcc213f434a54290423f7

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+const skiSchool = ["aki", "guadalupe", "lei", "aalam"];
+//const rollCall = [];
+
+// for(const student of skiSchool) {
+//   rollCall.push(student + " the skier");
+// }
+// console.log(skiSchool)
+// console.log(rollCall)
+
+const rollCall = skiSchool.map(function (student) {
+  return student + ' the skier';
+});
+
+console.log(rollCall)
